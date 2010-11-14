061e1978746e27a0b32c72e03972e9eb9420ea46
Eclipse- <sheppard.sam@gmail.com>
2010-11-14 23:42:56 +0000

    Gn.

--- Tukui/modules/kill.lua
+++ Tukui/modules/kill.lua
@@ -13,7 +13,7 @@ Kill:RegisterEvent("ADDON_LOADED")
 Kill:RegisterEvent("PLAYER_LOGIN")
 Kill:SetScript("OnEvent", function(self, event, addon)
 	if event == "PLAYER_LOGIN" then
-		if IsAddOnLoaded("Tukui_Dps_Layout") or IsAddOnLoaded("Tukui_Heal_Layout") then
+		if IsAddOnLoaded("TukuiDps") or IsAddOnLoaded("TukuiHeal") then
 			InterfaceOptionsFrameCategoriesButton10:SetScale(0.00001)
 			InterfaceOptionsFrameCategoriesButton10:SetAlpha(0)		
 			InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
